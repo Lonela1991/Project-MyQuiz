a90a3ade22b7c262b601872894d4ee83d14d0f8c
Louise <annica91louise@gmail.com>
2025-02-05 20:24:45 +0100

    added axios to call my APIs

--- src/components/QuestionListMedium.vue
+++ src/components/QuestionListMedium.vue
@@ -1,23 +1,35 @@
 <script>
-  export default {
-    created() {
-      fetch("https://opentdb.com/api.php?amount=10&category=32&difficulty=medium&type=multiple")
-        .then((response) => response.json())
-        .then((questions) => {
-          this.questions = questions.results;
-          
-        });
-    },
-    data() {
-      return {
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
       questions: null
-      };
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('https://opentdb.com/api.php?amount=10&category=32&difficulty=medium&type=multiple');
+      console.log(response);
+
+      this.questions = response.data.results.map((question) => {
+        return {
+          question: question.question,
+          correctAnswer: question.correct_answer,
+          incorrectAnswers: question.incorrect_answers
+        };
+      });
+      console.log(this.questions);
+    } 
+    catch (error) {
+      console.error(error);
     }
-  };
+  }
+};
 </script>
 
 <template>
   <ol>
-    <li v-for="question in questions">{{question}}</li>
+    <li v-for="question in questions" :key="question">{{ question.question }}</li>
   </ol>
 </template>
