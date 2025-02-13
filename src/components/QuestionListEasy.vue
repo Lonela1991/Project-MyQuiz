2b564d86e7b18182cdcfff8636c78673d251e24e
Louise <annica91louise@gmail.com>
2025-02-13 12:13:14 +0100

    css tillagt, samt sections, div och p element

--- src/components/QuestionListEasy.vue
+++ src/components/QuestionListEasy.vue
@@ -2,9 +2,9 @@
 import axios from 'axios'
 import { ref } from 'vue'
 
-let questions = ref([])
+const questions = ref([])
 
- axios.get('https://opentdb.com/api.php?amount=10&category=32&difficulty=easy&type=multiple')
+axios.get('https://opentdb.com/api.php?amount=10&category=32&difficulty=easy&type=multiple')
   .then(function(response){
     console.log(response.data.results)
 
@@ -13,17 +13,95 @@ let questions = ref([])
           correctAnswer: result.correct_answer,
           incorrectAnswers: result.incorrect_answers
       }));
-
       return questions
-
   })
+
+
 </script>
 
 <template>
-    <h2>Easy Questions</h2>
     <template v-for="question in questions" :key="question">
-     <dt>"Question: "{{question.question}}</dt>
-     <dt>"Correct answer: "{{question.correctAnswer}}</dt>
-     <dt>"Incorrect answer: "{{question.incorrectAnswers}}</dt>
+     <section id="questionSection">
+        <h2 id="questionText" >{{question.question}}</h2>
+     </section>
+     <section id="answerSection">
+        <div id="answerBox">
+        <p id="answerOption">{{question.correctAnswer}}</p>
+        </div>
+     </section>
     </template>
 </template>
+
+<style>
+body {
+  width: 100%;
+  background-color: #333;
+  color: #fff;
+  margin: 0px;
+}
+
+main {
+  width: 360px;
+}
+
+h1 {
+  font-family: sans-serif;
+}
+
+h2 {
+  font-family: sans-serif;
+  font-size: 16px;
+  color: black;
+}
+
+p {
+  font-family: sans-serif;
+}
+
+#questionSection {
+  background-color: white;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  height: 100px;
+  width: 100%;
+  line-height: 100px;
+  text-align: center;
+}
+
+#questionText {
+  line-height: 1.5;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+#answerSection {
+  display: flex;
+  background-color: grey;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-content: space-around;
+  height: 230px;
+  width: 100%;
+}
+
+#answerBox {
+  background-color: white;
+  border: 1px black;
+  height: 45%;
+  width: 45%;
+  line-height: 100px;
+  text-align: center;
+}
+
+#answerBox:active {
+  background-color: darkgrey;
+}
+
+#answerOption {
+  color: black;
+  line-height: 1.5;
+  display: inline-block;
+  vertical-align: middle;
+}
+</style>
